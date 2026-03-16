fe4cb65f9f2732f157650336a4cd1242af7aab46
Nik0la074 <nikola.stefanov074@gmail.com>
2026-03-16 13:58:32 +0200

    Add Reservation ViewModels

---
+++ RentACar.Web/ViewModels/CreateReservationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Web.ViewModels
+{
+    public class CreateReservationViewModel
+    {
+        [Required]
+        public int CarId { get; set; }
+
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ RentACar.Web/ViewModels/SearchCarsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Web.ViewModels
+{
+    public class SearchCarsViewModel
+    {
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; } = DateTime.Today.AddDays(1);
+
+        public IEnumerable<RentACar.Data.Models.Car> AvailableCars { get; set; }
+            = new List<RentACar.Data.Models.Car>();
+    }
+}
