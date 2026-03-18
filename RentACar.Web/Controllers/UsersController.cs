cc08a5c0237d025c0f3976b7a291d5838ecee3c8
Nik0la074 <nikola.stefanov074@gmail.com>
2026-03-18 10:23:06 +0200

    Add Users controller

---
+++ RentACar.Web/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Data.Models;
+using RentACar.Services.Interfaces;
+
+namespace RentACar.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(
+            IUserService userService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _userService = userService;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userService.GetAll();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var user = _userService.GetById(id);
+            if (user == null) return NotFound();
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ApplicationUser model)
+        {
+            var user = _userService.GetById(model.Id);
+            if (user == null) return NotFound();
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.PhoneNumber = model.PhoneNumber;
+            user.Email = model.Email;
+
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = _userService.GetById(id);
+            if (user == null) return NotFound();
+
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
